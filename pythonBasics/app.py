a166e99e735c13975d3162e3f1ea1f722535af48
Bharadwaj Vishnubhotla <vbharadwaj3456@gmail.com>
2023-11-22 04:37:57 +0530

    Create app.py

---
+++ pythonBasics/app.py
@@ -0,0 +1,21 @@
+# Triple quotes are used to write multi line string in python
+
+course_name = """ 
+Physics
+Maths
+Chemistry
+"""
+
+# initliaze multiple variables in same line
+x, y = 1, 2
+
+# Set multiple names to same values
+x = y = 1
+
+# C++, C#, Java are statically typed languages (Declare variable type when declaring)
+# Python, Javascript, Rudy are dynamically typed languages -->
+# (type of the variable is defined at run-time)
+
+# Numbers, Strings and Booleans are immutable
+# i.e, If we update the value, it will be stored in new memory location
+# Lists in python are mutable
