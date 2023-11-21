a85baec0a320ff0f9f4e92784e3be96be8e436df
Bharadwaj Vishnubhotla <vbharadwaj3456@gmail.com>
2023-11-22 04:22:58 +0530

    Update app.py

--- gettingStarted/app.py
+++ gettingStarted/app.py
@@ -1,2 +1,10 @@
 print("Hello World")
 print('*' * 10)
+
+# Python Execution
+# When Python code is executed using CPython -->
+# It converts the code into python Bytecode -->
+# Then it passed to Python Virtual Machine.. which converts it to Machine code
+
+# Jython - use Java code in python
+# Python code converted to Java Bytecode using Jython
