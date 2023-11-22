55ee7c35d73f027a82db30feec10c32e43ba7f0c
Bharadwaj Vishnubhotla <vbharadwaj3456@gmail.com>
2023-11-22 05:51:38 +0530

    Update strings.py

--- pythonBasics/strings.py
+++ pythonBasics/strings.py
@@ -1,4 +1,4 @@
-course = "Python Programming"
+course = "   Python Programming   "
 print(len(course))
 print(course[0])
 print(course[-1])  # first character from end of the list
@@ -8,3 +8,21 @@ print(course[0:3])  # print first three indexes
 print(course[:])  # print the entire string
 
 # Since strings are immutable, after all these operations --> Initial value remains same
+
+# Formatted Strings
+first = "Bharadwaj"
+last = "Vishnubhotla"
+
+full = f"{first} {last}"
+print(full)
+
+# Useful string operations
+print(course.upper())  # all characters to upper
+print(course.lower())  # all characters to lower
+print(course.title())  # first characters of everyword to upper
+print(course.strip())  # trim extra white spaces (starting or end of string)
+# --> we have lstrip and rstrip too
+print(course.find("Pro"))  # returns Index of Pro
+# --> If exact match not found.. returns -1
+print(course.find("pro"))
+print(course.replace("P", "-"))  # replace all P with -
