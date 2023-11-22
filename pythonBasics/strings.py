2886b715ab820ee9c48886b917ca0f22759d74d3
Bharadwaj Vishnubhotla <vbharadwaj3456@gmail.com>
2023-11-22 05:41:18 +0530

    Create strings.py

---
+++ pythonBasics/strings.py
@@ -0,0 +1,10 @@
+course = "Python Programming"
+print(len(course))
+print(course[0])
+print(course[-1])  # first character from end of the list
+
+# Slicing a string
+print(course[0:3])  # print first three indexes
+print(course[:])  # print the entire string
+
+# Since strings are immutable, after all these operations --> Initial value remains same
