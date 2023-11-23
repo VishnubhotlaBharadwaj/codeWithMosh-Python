2fe448378bd3465fa542d12c665cd6a43c874936
Bharadwaj Vishnubhotla <vbharadwaj3456@gmail.com>
2023-11-23 22:53:16 +0530

    Create numbers.py

---
+++ pythonBasics/numbers.py
@@ -0,0 +1,8 @@
+# to print immaginery numbers
+x = 1 + 2j  # j is like i in immaginery numbers
+print(x)
+
+# Division operation returns Quotient
+# Modulo operation returns Remainder
+# '/' performs divison
+# '//' performs integer division
