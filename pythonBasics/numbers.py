c8d6f6260eb69773156fb67fa03454342e59a0f5
Bharadwaj Vishnubhotla <vbharadwaj3456@gmail.com>
2023-11-29 17:36:11 +0530

    Update numbers.py

--- pythonBasics/numbers.py
+++ pythonBasics/numbers.py
@@ -1,8 +1,16 @@
+
+import math
+
 # to print immaginery numbers
-x = 1 + 2j  # j is like i in immaginery numbers
+x = 1 + 2j  # j is like i in imaginery numbers
 print(x)
 
 # Division operation returns Quotient
 # Modulo operation returns Remainder
 # '/' performs divison
 # '//' performs integer division
+# Augmented assignment operator --> +=, -=
+print(round(2.9))  # rounds to the nearest integer
+print(abs(-2.9))  # prints absolute value of number
+
+print(math.ceil(2.2))  # prints the next integer
