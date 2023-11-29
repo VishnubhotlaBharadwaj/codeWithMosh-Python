58b7b601b49fb4c08d02f88d3dec1f2350ed35a4
Bharadwaj Vishnubhotla <vbharadwaj3456@gmail.com>
2023-11-29 20:17:39 +0530

    Update app.py

---
+++ functions/app.py
@@ -0,0 +1,11 @@
+def greet(first_name, last_name):
+    print(f"Hi {first_name} {last_name}")
+    print("Welcome aboard")
+
+
+greet("Bharadwaj", "Vishnubhotla")
+greet("John", "Smith")
+
+# parameter is the input defined for the function
+# argument is the actual value of the parameter
+# functions are of two types --> 1. Perform a trask, 2. Return a value
