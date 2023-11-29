b25b03cef6f83be7bdeb597ec077d816c6a67361
Bharadwaj Vishnubhotla <vbharadwaj3456@gmail.com>
2023-11-29 18:13:10 +0530

    Create app.py

---
+++ controlFlow/app.py
@@ -0,0 +1,10 @@
+# ord() --> gives the numeric reperesentation of the letter
+# condition is always an boolean expression
+# if - elif - else
+
+# ternary operator
+age = 16
+message = "Eligible" if age >= 18 else "Not eligibile"
+print(message)
+
+# Logical operators --> 'or' ; 'and' ; 'not'
