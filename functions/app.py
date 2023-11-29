04709a82cd863ad0e4a99340809f82708abfe7b6
Bharadwaj Vishnubhotla <vbharadwaj3456@gmail.com>
2023-11-29 20:35:36 +0530

    Update app.py

--- functions/app.py
+++ functions/app.py
@@ -9,3 +9,33 @@ greet("John", "Smith")
 # parameter is the input defined for the function
 # argument is the actual value of the parameter
 # functions are of two types --> 1. Perform a trask, 2. Return a value
+# In python all functions returns the value none, unless you specifically returns a value
+
+# keyword arguments
+
+
+def increment(number, by):
+    return number + by
+
+
+print(increment(2, by=1))
+
+# default arguments
+
+
+def increment(number, by=1):
+    return number + by
+
+
+print(increment(2, 7))
+
+# variable number of arguments (xargs)
+
+
+def multiply(*numbers):
+    print(numbers)
+    for number in numbers:
+        print(number)
+
+
+multiply(2, 3, 4, 5)
