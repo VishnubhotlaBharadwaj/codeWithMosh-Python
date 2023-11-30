be70871450550b576e0431516da198ea516e283d
Bharadwaj Vishnubhotla <vbharadwaj3456@gmail.com>
2023-11-30 17:34:31 +0530

    Create others.py

---
+++ dataStructures/others.py
@@ -0,0 +1,11 @@
+# Stack --> Last In First Out (LIFO)
+# .append() to add items in the last
+# .pop() to remove items from the last
+
+# Queue --> First IN First Out (FIFO)
+# If there are many objects in the queue, removing the first element make the other elements to shift one position left which leads to performance issues
+# To tackle the above problem --> we use Dequeue method
+# to remove first element --> .popleft() is used
+
+# Tuples --> read only list, we cannot modify existing objects
+# () is used to define a tuple
