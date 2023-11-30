f40bb9e2c9de1ebb704ac081dd17d5ea61622000
Bharadwaj Vishnubhotla <vbharadwaj3456@gmail.com>
2023-11-30 16:50:47 +0530

    Update lists.py

--- dataStructures/lists.py
+++ dataStructures/lists.py
@@ -8,9 +8,20 @@ zeros = [0] * 10  # prints 10 zeros
 # list function is iterable
 list(range(20))  # prints a list from 0 to 19
 
-len(list)  # gives length of the list
+a = len(zeros)  # gives length of the list
 
 # list is mutable
 # during slicing a string we can pass a step too
 l = [1, 2, 3, 4, 5, 6]
 print(l[::2])
+
+# list unpacking
+# we can assign values of a list to different variable
+# but, no. of variables in the left side should be equal to number of values in list
+numbers = [1, 2, 3]
+first, second, third = numbers
+
+# we can also pack the unnecessary values
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# prints first, pack all other into seperate list, prints last
+first, *other, last = numbers
