8280125a21e525b1a83ace47a2c0ead6e4df5527
Bharadwaj Vishnubhotla <vbharadwaj3456@gmail.com>
2023-12-01 10:15:42 +0530

    Update app.py

--- classes/app.py
+++ classes/app.py
@@ -6,11 +6,18 @@
 # classes uses PascalNamingConvention
 
 class Point:
+
+    def __init__(self, x, y):  # constructor; self is the reference for current object
+        self.x = x
+        self.y = y
+
     def draw(self):  # all functions in the classes should have atleast one parameter
-        print("draw")
+        print(f" Point ({self.x}, {self.y})")
 
 
-point = Point()
+point = Point(1, 2)
 point.draw()
 print(type(point))
 print(isinstance(point, Point))
+print(point.x)
+print(point.y)
