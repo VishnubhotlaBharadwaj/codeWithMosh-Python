368d97b6a48b585c12d7d5cc37e5539dd6ce5ea7
Bharadwaj Vishnubhotla <vbharadwaj3456@gmail.com>
2023-12-01 10:12:12 +0530

    Update app.py

--- classes/app.py
+++ classes/app.py
@@ -3,3 +3,14 @@
 # Example:
 # Class: Human
 # Objects: John, Mary, jack etc..
+# classes uses PascalNamingConvention
+
+class Point:
+    def draw(self):  # all functions in the classes should have atleast one parameter
+        print("draw")
+
+
+point = Point()
+point.draw()
+print(type(point))
+print(isinstance(point, Point))
